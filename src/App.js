2677d3b618cc096ada77f778df66035ed727cf7a
Zachary Olpin <info@zacharyolp.in>
2020-09-13 01:33:47 -0600

    temp font and basic mobile styling + cute svg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import Leafs from '../public/img/leafs-maybe.svg';
 
 const App = () => {
-  return <div>Heyo</div>
+  return (
+    <>
+      <h1>BROOMSTICK</h1>
+        <p>
+          Lorem Ipsum something dolor something ipset barndoor wagon racecar living discgolf zipping weather flip dive tray rider spare rib with it. Dog's white teeth sliced right through my wrist. Christmas Eve with yr mother and sis. Don't wanna fight but yr mother insists.
+        </p>
+        <object type="image/svg+xml" data={Leafs}>Your browser does not support SVGs</object>
+    </>
+  );
 }
 
 export { App };
